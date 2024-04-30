aa3863db932629b1a42bfa0bed8b376afdc97633
Giacomo-Cattani <giacomo.cattani@stu.fondazionejobsacademy.org>
2024-04-30 18:15:16 +0200

    fix: padding

--- src/components/LeftCard.tsx
+++ src/components/LeftCard.tsx
@@ -13,12 +13,12 @@ function LeftCard() {
             <Card style={{ backgroundColor: '#1D2837', color: 'white', margin: '0 auto', boxShadow: '12px 10px 10px rgba(0,0,0, .5)', height: '100%' }}>
                 <CardContent style={{ display: 'flex', flexDirection: 'column' }}>
                     <Grid container rowSpacing={2} display={"flex"} justifyContent={"space-between"} alignItems={"flex-end"} marginTop={2}>
-                        <Grid item marginLeft={2} marginTop={2}>
+                        <Grid item marginLeft={2}>
                             <Typography variant="h4" gutterBottom align="left">
                                 Locality
                             </Typography>
                         </Grid>
-                        <Grid item marginTop={2} marginRight={2}>
+                        <Grid item marginRight={2}>
                             <Typography variant="h6" gutterBottom align="right">
                                 Time
                             </Typography>
