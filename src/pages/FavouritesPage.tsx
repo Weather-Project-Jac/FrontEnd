1fd62dedb9deff9c1f115890bfabfe32ea6206cd
Giacomo-Cattani <giacomo.cattani@stu.fondazionejobsacademy.org>
2024-05-01 12:19:05 +0200

    fix: some paddings, and general style of the favourite page feat: add variable and function in store page for handle adding and removing cities from array

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -8,10 +8,17 @@ import {
 } from "@mui/material";
 import LeftCard from "../components/LeftCard";
 import { display } from "@mui/system";
+import icons from "../assets/icons";
+import { UserStore } from "../store/store";
 
 function HomePage() {
+    const favouriteCities = UserStore((state) => state.addFavouriteCities);
+    const removeFavourite = UserStore((state) => state.removeFavouriteCities);
+    console.log("Added? ", favouriteCities("Ciao"))
+    console.log("Removed? ", removeFavourite("Ciao"))
+
     return (
-        <Container maxWidth="xl" style={{ display: 'flex' }}>
+        <Container maxWidth="xl" >
             <Grid
                 container
                 justifyContent="space-around"
@@ -21,45 +28,47 @@ function HomePage() {
             >
                 <LeftCard />
 
-
-                {/* 
-                Fixing column to row 
-                <Grid container style={{ display: "flex", justifyContent: "space-evenly", flexDirection: 'column' }}>
-                    {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].map((items) => (
-                        <Grid item xs={4} style={{ textAlign: "center", paddingLeft: 0 }}>
-                            <Typography variant="h6">{items}</Typography>
-                            <Typography style={{
-                                fontSize: 30,
-                                fontWeight: 600
-                            }}></Typography>
-                        </Grid>
-                    ))}
-                </Grid> */}
-
-                <Grid item xs={4} sm={7} justifyContent="space-evenly" >
-                    <Card style={{ color: 'white', backgroundColor: '#0A2239', borderRadius: '15px' }}>
-                        <CardContent style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                            <Typography gutterBottom variant="h5" component="div">
-                                Card Title 6
-                            </Typography>
-                            <Box component='img'
-                                sx={{
-                                    display: 'block',
-                                    height: 233,
-                                    width: '100%', // Make the image box span the full width
-                                    maxHeight: { xs: 233, md: 167 },
-                                    maxWidth: { xs: 350, md: 250 },
-                                    margin: 'auto',
-                                }} src="https://placehold.co/400" />
-                            <Typography variant="body1" color="white" sx={{ fontWeight: 'bold' }}>
-                                24°
-                            </Typography>
-                            <Typography variant="caption" color="white">
-                                Sunny
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                <Grid item xs={12} sm={7} style={{ paddingLeft: 0 }}>
+                    <Grid container spacing={10} style={{ height: '90vh', paddingTop: 50, paddingBottom: 50, paddingLeft: 130 }} alignItems="center" display={'flex'} >
+                        {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].map((items) => (
+                            <Grid item xs={12} sm={4} key={items}>
+                                <Card style={{ color: 'white', backgroundColor: '#1D2837', borderRadius: '15px', boxShadow: '12px 10px 10px rgba(0,0,0, .5)' }}>
+                                    <CardContent style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', textAlign: 'center' }}>
+                                        <Typography gutterBottom variant="h5" component="div" >
+                                            Locality
+                                        </Typography>
+                                        <Box
+                                            component='img'
+                                            sx={{
+                                                // display: 'block',
+                                                width: '70%',
+                                                margin: 'auto',
+                                            }}
+                                            src={icons.clear} />
+                                        <Typography variant="body1" color="white" sx={{
+                                            pl: 1,
+                                            textAlign: 'center',
+                                            fontSize: 30,
+                                            fontFamily: "Inter, sans-serif",
+                                            fontWeight: 600
+                                        }}>
+                                            24°
+                                        </Typography>
+                                        <Typography variant="body1" color="white" sx={{
+                                            textAlign: 'center',
+                                            fontSize: 20,
+                                            fontFamily: "Inter, sans-serif",
+                                            fontWeight: 300
+                                        }}>
+                                            Sunny
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Grid>
+
             </Grid>
         </Container >
     );
